3340d34f3e7d47c457fa59d07a68cde7b8f58280
AltrandGD <161524371+AltrandGD@users.noreply.github.com>
2026-08-24 00:43:20 +0300

    Fedor's Comment

--- Assets/Scripts/Controller/BotController.cs
+++ Assets/Scripts/Controller/BotController.cs
@@ -6,9 +6,7 @@ using Model;
 using Model.Config;
 using UnityEngine;
 using Utilities;
-//hello
-//How are you?
-//I'm fine
+// hello, I'm Fedor
 namespace Controller
 {
     public class BotController
